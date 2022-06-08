5a7c9e5feec4a807fe3b3f41b61152993e10a4e0
Adalberto Junior <adljunior@rd.com.br>
2022-06-07 22:05:54 -0300

    Implementation Tab Bar

--- App.tsx
+++ App.tsx
@@ -13,6 +13,9 @@ import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
 import { CategorySelect } from './src/screens/CategorySelect';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,8 +30,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
-
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
